272f035c969833b8dc59fcff7bed30a9d14cb006
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2024-06-05 22:00:14 -0300

    DI Container

--- src/Tests/index.php
+++ src/Tests/index.php
@@ -3,12 +3,6 @@
 require_once '../../vendor/autoload.php';
 
 use MiniRestFramework\Core\App;
-use MiniRestFramework\Router\Router;
-use MiniRestFramework\Tests\Objects\ExampleController;
 
 $app = new App();
-
-Router::get('/test', [ExampleController::class, 'handleRequest']);
-
-
 $app->run();
