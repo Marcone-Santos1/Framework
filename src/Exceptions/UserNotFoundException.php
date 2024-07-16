6771f51d2dc849791f2d1a63901356278ce81d43
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2024-07-16 17:39:30 -0300

    fix: exceptions

--- src/Exceptions/UserNotFoundException.php
+++ src/Exceptions/UserNotFoundException.php
@@ -6,8 +6,8 @@ use MiniRestFramework\Helpers\StatusCode\StatusCode;
 
 class UserNotFoundException extends \Exception
 {
-    public function __construct(string $message = 'User not found!', int $code = StatusCode::NOT_FOUND, \Exception $previous = null)
+    public function __construct(string $message = 'User not found')
     {
-        parent::__construct($message, $code);
+        parent::__construct($message, StatusCode::ACCESS_NOT_ALLOWED);
     }
 }
